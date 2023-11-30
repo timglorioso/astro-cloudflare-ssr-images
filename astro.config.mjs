5005365c2b7dcbbb814a7acd5b21538541c6deff
Tim Glorioso <elegant.art2953@fastmail.com>
2023-11-30 12:57:59 -0500

    Use server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   image: {
     domains: ["a-us.storyblok.com"],
     service: passthroughImageService(),
